9115fa307a32d0d867039cb5d4085fe72bc5a08a
William <William.Nkuna@boxfusion.io>
2022-09-17 20:36:08 +0200

    Add project files.

---
+++ SuperHeroAPI/Data/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SuperHeroAPI.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        // Because we use ...migration and we wanna see a representation of our super heroes in the database.
+         
+        public DbSet<SuperHero> SuperHeroes { get; set; } // in plural, "SuperHeroes" will be the name of our table.
+    }
+}
